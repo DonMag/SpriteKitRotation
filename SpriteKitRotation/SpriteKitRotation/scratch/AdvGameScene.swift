40f5fc773ade5352f9523ec97c3f8674a73e04ae
DonMag <don_mail_2000@yahoo.com>
2024-04-21 16:14:52 -0400

    move image extension to Utilities

--- SpriteKitRotation/SpriteKitRotation/scratch/AdvGameScene.swift
+++ SpriteKitRotation/SpriteKitRotation/scratch/AdvGameScene.swift
@@ -10,34 +10,6 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
-extension UIImage {
-	
-	func rotated(byDegrees degrees: CGFloat) -> UIImage? {
-		let renderer = UIGraphicsImageRenderer(size: size)
-		return renderer.image { context in
-			// Translate and rotate the context
-			context.cgContext.translateBy(x: size.width / 2, y: size.height / 2)
-			context.cgContext.rotate(by: degrees * .pi / 180.0)
-			
-			// Draw the image into the context
-			draw(in: CGRect(x: -size.width / 2, y: -size.height / 2, width: size.width, height: size.height))
-		}
-	}
-
-	func horizontallyMirrored() -> UIImage? {
-		let renderer = UIGraphicsImageRenderer(size: size)
-		return renderer.image { context in
-			// Flip the context horizontally
-			context.cgContext.translateBy(x: size.width, y: 0)
-			context.cgContext.scaleBy(x: -1, y: 1)
-			
-			// Draw the image into the context
-			draw(in: CGRect(origin: .zero, size: size))
-		}
-	}
-
-}
-
 class AdvGameScene: SKScene {
 
 	var ellipsePortrait: MyEllipse = MyEllipse()
